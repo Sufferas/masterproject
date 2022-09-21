baf9795c550fbb23d6992a6643a7394bb4540de2
Alexander Kühhas <alexander.kuehhas@tttech-auto.com>
2022-09-21 14:34:44 +0200

    NOISSUE-1 add gui exaples not finish

--- deepLearning/chat.py
+++ deepLearning/chat.py
@@ -29,76 +29,76 @@ model.eval()
 
 bot_name = "Sam"
 print("Let's chat! (type 'quit' to exit)")
-# while True:
-#     # sentence = "do you use credit cards?"
-#     sentence = input("You: ")
-#     if sentence == "quit":
-#         break
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        print(prob.item())
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(prob.item())
+        print(f"{bot_name}: I do not understand...")
+
+# test_text = ["Ein besonderer Test", "welcher Spezifikation benötigt ein Unit Test beim schönem Wetter",
+#              "Zeit zum schlafen oder?","was machst du gerade?",
+#              "wir haben ein traumhaftes Wetter oder", "was machst du heute",
+#              "was soll ich mir kaufen","gibt es heute was Besonderes im Kino", "was benötige ich um ein Spiel zu spielen",
+#              "hello and bye"]
 #
-#     sentence = tokenize(sentence)
-#     X = bag_of_words(sentence, all_words)
-#     X = X.reshape(1, X.shape[0])
-#     X = torch.from_numpy(X).to(device)
 #
-#     output = model(X)
-#     _, predicted = torch.max(output, dim=1)
+# file = open('../../Sem2_project_master_arbeit/example_csv.csv', 'w')
 #
-#     tag = tags[predicted.item()]
+# with file:
+#     writer = csv.writer(file)
+#     writer.writerow(("ID", "Test Satz", "Genauigkeit 0.00 - 1", "Ausgabe des Chatbots"))
 #
-#     probs = torch.softmax(output, dim=1)
-#     prob = probs[0][predicted.item()]
-#     if prob.item() > 0.75:
-#         print(prob.item())
-#         for intent in intents['intents']:
-#             if tag == intent["tag"]:
-#                 print(f"{bot_name}: {random.choice(intent['responses'])}")
-#     else:
-#         print(prob.item())
-#         print(f"{bot_name}: I do not understand...")
-
-test_text = ["Ein besonderer Test", "welcher Spezifikation benötigt ein Unit Test beim schönem Wetter",
-             "Zeit zum schlafen oder?","was machst du gerade?",
-             "wir haben ein traumhaftes Wetter oder", "was machst du heute",
-             "was soll ich mir kaufen","gibt es heute was Besonderes im Kino", "was benötige ich um ein Spiel zu spielen",
-             "hello and bye"]
-
-
-file = open('../../Sem2_project_master_arbeit/example_csv.csv', 'w')
-
-with file:
-    writer = csv.writer(file)
-    writer.writerow(("ID", "Test Satz", "Genauigkeit 0.00 - 1", "Ausgabe des Chatbots"))
-
-    id_num = 1
-    for x in test_text:
-        # sentence = "do you use credit cards?"
-
-        sentence = x
-        test = ""
-
-        sentence = tokenize(sentence)
-        X = bag_of_words(sentence, all_words)
-        X = X.reshape(1, X.shape[0])
-        X = torch.from_numpy(X).to(device)
-
-        output = model(X)
-        _, predicted = torch.max(output, dim=1)
-
-        tag = tags[predicted.item()]
-
-        probs = torch.softmax(output, dim=1)
-        prob = probs[0][predicted.item()]
-        if prob.item() > 0.75:
-            print(prob.item())
-            for intent in intents['intents']:
-                if tag == intent["tag"]:
-                    print(f"{bot_name}: {random.choice(intent['responses'])}")
-                    test = random.choice(intent['responses'])
-        else:
-            print(prob.item())
-            print(f"{bot_name}: I do not understand...")
-            test = "I do not understand..."
-
-        num = str(prob.item()).replace(".", ",")
-        writer.writerow((id_num,x,num,test))
-        id_num = id_num + 1
+#     id_num = 1
+#     for x in test_text:
+#         # sentence = "do you use credit cards?"
+#
+#         sentence = x
+#         test = ""
+#
+#         sentence = tokenize(sentence)
+#         X = bag_of_words(sentence, all_words)
+#         X = X.reshape(1, X.shape[0])
+#         X = torch.from_numpy(X).to(device)
+#
+#         output = model(X)
+#         _, predicted = torch.max(output, dim=1)
+#
+#         tag = tags[predicted.item()]
+#
+#         probs = torch.softmax(output, dim=1)
+#         prob = probs[0][predicted.item()]
+#         if prob.item() > 0.75:
+#             print(prob.item())
+#             for intent in intents['intents']:
+#                 if tag == intent["tag"]:
+#                     print(f"{bot_name}: {random.choice(intent['responses'])}")
+#                     test = random.choice(intent['responses'])
+#         else:
+#             print(prob.item())
+#             print(f"{bot_name}: I do not understand...")
+#             test = "I do not understand..."
+#
+#         num = str(prob.item()).replace(".", ",")
+#         writer.writerow((id_num,x,num,test))
+#         id_num = id_num + 1
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from edit_file import open_read_file, open_write_file
 from speech_record import from_microphone
-
+from tkinter import *
 
 def start_point():
     """
@@ -28,17 +28,61 @@ def start_point():
             return
 
 
+import time
+import tkinter as tk
+import threading
+from pubsub import pub
+
+lock = threading.Lock()
 
 
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.label = tk.Label(root, text="Temperature / Humidity")
+        self.label.pack(side="top", fill="both", expand=True)
+
+    def listener(self, plot_data):
+        with lock:
+            """do your plot drawing things here"""
+            self.label.configure(text=plot_data)
+
+
+class WorkerThread(threading.Thread):
+    def __init__(self):
+        super(WorkerThread, self).__init__()
+        self.daemon = True  # do not keep thread after app exit
+        self._stop = False
+
+    def run(self):
+        """calculate your plot data here"""
+        for i in range(100):
+            if self._stop:
+                break
+            time.sleep(1)
+            pub.sendMessage('update', text=str(i))
 
 
 if __name__ == '__main__':
+    root = tk.Tk()
+    root.wm_geometry("320x240+100+100")
+
+    main = MainApplication(root)
+    main.pack(side="top", fill="both", expand=True)
+
+    pub.subscribe(main.listener, 'update')
+
+    wt = WorkerThread()
+    wt.start()
+
+    root.mainloop()
 
     print("Welcome, you can now edit files or codes also via voice control.")
     from_microphone()
 
-    while True:
-        start_point()
+    # while True:
+    #     start_point()
 
 
     # open_read_file('File2.py')
