5e154567aab06bbf54788661b286eae9c043504a
Alexander Kühhas <alexander.kuehhas@stud.fh-campuswien.ac.at>
2022-09-21 20:14:41 +0200

    edit gui add first steps

--- main.py
+++ main.py
@@ -27,56 +27,8 @@ def start_point():
         if "edit new file" == "edit new file": # replace "edit new file" with a Command
             return
 
-
-import time
-import tkinter as tk
-import threading
-from pubsub import pub
-
-lock = threading.Lock()
-
-
-class MainApplication(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.label = tk.Label(root, text="Temperature / Humidity")
-        self.label.pack(side="top", fill="both", expand=True)
-
-    def listener(self, plot_data):
-        with lock:
-            """do your plot drawing things here"""
-            self.label.configure(text=plot_data)
-
-
-class WorkerThread(threading.Thread):
-    def __init__(self):
-        super(WorkerThread, self).__init__()
-        self.daemon = True  # do not keep thread after app exit
-        self._stop = False
-
-    def run(self):
-        """calculate your plot data here"""
-        for i in range(100):
-            if self._stop:
-                break
-            time.sleep(1)
-            pub.sendMessage('update', text=str(i))
-
-
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.wm_geometry("320x240+100+100")
 
-    main = MainApplication(root)
-    main.pack(side="top", fill="both", expand=True)
-
-    pub.subscribe(main.listener, 'update')
-
-    wt = WorkerThread()
-    wt.start()
-
-    root.mainloop()
 
     print("Welcome, you can now edit files or codes also via voice control.")
     from_microphone()
--- tkinterapp.py
+++ tkinterapp.py
@@ -7,44 +7,59 @@ from random import randint
 def rando(random_label):
     random_label.config(text=f'Random Number: {randint(1,100)}')
 
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("270x200")
-        self.title('Login')
+        self.geometry("1200x1000")
+        self.title('voice controlled editor')
         self.resizable(0, 0)
 
         # configure the grid
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
+        self.columnconfigure(1, weight=10)
 
         self.create_widgets()
 
     def create_widgets(self):
-        # username
-        username_label = ttk.Label(self, text="Username:")
-        username_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        dict_tk = {}
+        rules = ttk.Label(self, text="Momentary task", style='Heading.TLabel')
+        rules.grid(column=0, row=0, sticky=tk.N, padx=5, pady=5, columnspan=2)
 
-        username_entry = ttk.Entry(self)
-        username_entry.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
+        edit_file_label = ttk.Label(self, text="File to edit: ")
+        edit_file_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
 
-        # password
-        password_label = ttk.Label(self, text="Password:")
-        password_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        edit_file_text = ttk.Label(self, text="no file selected: tell the file direktory, name and the format. ")
+        edit_file_text.grid(column=1, row=1, sticky=tk.W, padx=5, pady=5)
+        dict_tk["edit_file_text"] = edit_file_text
 
-        password_entry = ttk.Entry(self,  show="*")
-        password_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+        line_being_edited_label = ttk.Label(self, text="line being edited: x")
+        line_being_edited_label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+        dict_tk["line_being_edited_label"] = line_being_edited_label
+        line_being_edited_text = ttk.Label(self, text="")
+        line_being_edited_text.grid(column=1, row=2, sticky=tk.W, padx=5, pady=5)
+        dict_tk["line_being_edited_text"] = line_being_edited_text
 
-        # login button
-        login_button = ttk.Button(self, text="Login")
-        login_button.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
+        voice_recording_output_label = ttk.Label(self, text="voice recording output: ")
+        voice_recording_output_label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+        voice_recording_output_text = ttk.Label(self, text="")
+        voice_recording_output_text.grid(column=1, row=3, sticky=tk.W, padx=5, pady=5)
+        dict_tk["voice_recording_output_text"] = voice_recording_output_text
 
-        random_label = ttk.Label(self, text="Test")
-        random_label.grid(column=0, row=4, sticky=tk.W, padx=5, pady=5)
+        system_help_head = ttk.Label(self, text="The system requires special conmandos in some cases so that no information is lost and can be stored.", style='Heading.TLabel')
+        system_help_head.grid(column=0, row=4, sticky=tk.N, padx=5, pady=5, columnspan=2)
+        system_help_label = ttk.Label(self, text="System says: ")
+        system_help_label.grid(column=0, row=5, sticky=tk.W, padx=5, pady=5, columnspan=2)
+        system_help_text = ttk.Label(self, text="System says: ")
+        system_help_text.grid(column=1, row=5, sticky=tk.W, padx=5, pady=5, columnspan=2)
+        dict_tk["system_help_text"] = system_help_text
 
-        random_label2 = ttk.Label(self, text="Test2", command=rando(random_label))
-        random_label2.grid(column=0, row=5, sticky=tk.W, padx=5, pady=5)
+
+        self.style = ttk.Style(self)
+        self.style.configure('Heading.TLabel', font=('Helvetica', 12))
+
+        # print(dict_tk)
 
 
 if __name__ == "__main__":
